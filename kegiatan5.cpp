bf4809530d9831e71f36769d21d4ff90920e3efe
RezaDwiSaputra <101698561+rezadwisaputra2100018257@users.noreply.github.com>
2022-04-20 22:11:37 -0700

    Add files via upload

---
+++ kegiatan5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class perulangan{
+	public:
+		perulangan() {n=4;}
+		void ulang_for(int);
+		void ulang_rekursif(int);
+	private:
+		int n;
+};
+void perulangan::ulang_for(int n)
+{
+	for(int i=1; i<=n; i++)
+	cout<<i<<" ";
+	getch();
+}
+void perulangan::ulang_rekursif(int n){
+	int i=4;
+	while(i>=n){
+		cout<<i<<" "; 
+    i--;}
+    getch();
+}
+main(){
+	perulangan X;
+	cout<<"Menggunakan cara iteratif\n";
+	X.ulang_for(4);
+	cout<<"\nMenggunakan cara rekursif\n";
+	X.ulang_rekursif(4);
+}
